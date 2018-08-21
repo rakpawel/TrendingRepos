a169a6f72f316cfea3b2cc046c7ce32e97623cea
Pawel Rak <pawelrak3@gmail.com>
2018-08-21 23:55:24 +0200

    simple details view

--- app/src/main/java/com/pawelrak/trendingrepos/data/model/GithubRepository.kt
+++ app/src/main/java/com/pawelrak/trendingrepos/data/model/GithubRepository.kt
@@ -1,7 +1,14 @@
 package com.pawelrak.trendingrepos.data.model
 
 import android.os.Parcelable
+import com.squareup.moshi.Json
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class GithubRepository (val name: String) : Parcelable
+data class GithubRepository(
+        @Json(name = "name") val name: String,
+        @Json(name = "full_name") val fullName: String,
+        @Json(name = "language") val language: String,
+        @Json(name = "description") val description: String,
+        @Json(name = "stargazers_count") val stargazers: Int,
+        @Json(name = "watchers_count") val watchers: Int) : Parcelable
